566916d33751baa51e2e6c05d2e9c8f86415fa1c
Kevin Martinez <kevinmartinez130605@gmail.com>
2024-11-25 16:35:43 -0500

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,12 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            {{-- <x-jet-authentication-card-logo /> --}}
-            <div class="flex items-center justify-center">
-                <div class="flex flex-col items-center">
-                    <a href="" class="mx-6 my-2">
-                        <span class="text-4xl text-gray-500 cursor-pointer font-bold">Book</span>
-                        <span class="text-4xl font-bold text-gray-500 cursor-pointer">Shop</span>
-                    </a>
-                    <a href="">
-                        <span class="text-4xl font-bold text-blue-500 cursor-pointer">GALEN</span>
-                    </a>
-                </div>
-            </div>
+            <img src="{{ asset('img/LIBRERIA.svg') }}" alt="" width="600px">
         </x-slot>
 
         {{-- <x-jet-validation-errors class="mb-4" /> --}}
+        <h2 class=" text-5xl  font-bold text-blue-950 mb-16 mt-5 ">Iniciar Sesión</h2>
+
         <x-validation-errors class="mb-4" />
 
         @session('status')
@@ -28,31 +19,31 @@
             @csrf
 
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
+                <x-label for="email" value="{{ __('Correo Electrónico') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
+                <x-label for="password" value="{{ __('Contraseña') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ms-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-end mt-10 ">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('¿Olvidaste tu contraseña?') }}
                     </a>
                 @endif
 
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
+                <x-button class="ms-4 bg-[#2684b6] hover:bg-[#1c92d2]">
+                    {{ __('Iniciar Sesión') }}
                 </x-button>
             </div>
         </form>
--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,13 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
+<div
+    class="min-h-screen flex  sm:justify-center items-center pt-6 sm:pt-0 bg-gradient-to-r from-[#f2fcfe] to-[#1c92d2]">
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+    <div class="flex space-x-20 items-center ">
+        <div>
+            {{ $logo }}
+        </div>
+
+        <div class="w-full h-full sm:max-w-md mt-6 px-6 py-4 bg-white  overflow-hidden sm:rounded-2xl shadow-2xl">
+            {{ $slot }}
+        </div>
     </div>
 </div>
