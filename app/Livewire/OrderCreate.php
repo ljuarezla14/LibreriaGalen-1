541ffd9558f4d5aa513dadb7f1f2a7089eb4e702
Brayan Martinez <brayanmartinezojeda0@gmail.com>
2024-11-27 17:34:56 -0500

    ultimos cambios v5

--- app/Livewire/OrderCreate.php
+++ app/Livewire/OrderCreate.php
@@ -5,7 +5,7 @@ namespace App\Livewire;
 use App\Models\Order;
 use App\Models\Products;
 use CodersFree\Shoppingcart\Facades\Cart;
-use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
@@ -53,13 +53,42 @@ class OrderCreate extends Component
         $this->dni = $order->dni;
         $this->phone = $order->phone;
         $this->status = $order->status;
-        if(Cart::content()){
-            $order->content = Cart::content();
-            $order->total = Cart::subtotal();
-            $order->save();
-            Cart::destroy();
+        // if(Cart::content()){
+        //     $order->content = Cart::content();
+        //     $order->total = Cart::subtotal();
+        //     $order->save();
+        //     Cart::destroy();
+        // }
+
+        foreach (Cart::content() as $cartItem) {
+            $product = Products::find($cartItem->id); // Buscar el producto por ID
+
+            if ($product) {
+                // Verificar si el producto tiene suficiente stock
+                if ($product->quantity >= $cartItem->qty) {
+                    // Descontar la cantidad del stock
+                    $product->quantity -= $cartItem->qty;
+                    $product->save(); // Guardar la actualización en la base de datos
+                    if($product->quantity == 0){
+                        $product->status = 1;
+                        $product->save();
+                    }
+                    // Crear la orden
+                    $this->client = $order->client;
+                    $this->dni = $order->dni;
+                    $this->phone = $order->phone;
+                    $this->status = $order->status;
+                    $order->user_id = Auth::user()->id;
+                    $order->total = Cart::subtotal();
+                    $order->content = Cart::content();
+                    $order->save();
+                }
+
+            }
         }
+        Cart::destroy();
         $items = json_decode($order->content);
+        $this->dispatch('render');
         return view('livewire.order-create', compact('order', 'items'));
     }
 
